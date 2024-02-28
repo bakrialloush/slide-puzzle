f57a5ee528a56e91283aff4860844267052f09cf
Bakri <backar-net@hotmail.com>
2024-02-28 17:52:28 +0300

    Add project files.

---
+++ Puzzle/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.BtnRandomize = new System.Windows.Forms.Button();
+            this.LabelClicks = new System.Windows.Forms.Label();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.table.ColumnCount = 1;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.table.Location = new System.Drawing.Point(196, 64);
+            this.table.Name = "table";
+            this.table.RowCount = 1;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.table.Size = new System.Drawing.Size(307, 318);
+            this.table.TabIndex = 0;
+            // 
+            // BtnRandomize
+            // 
+            this.BtnRandomize.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BtnRandomize.Location = new System.Drawing.Point(80, 117);
+            this.BtnRandomize.Name = "BtnRandomize";
+            this.BtnRandomize.Size = new System.Drawing.Size(91, 38);
+            this.BtnRandomize.TabIndex = 1;
+            this.BtnRandomize.Text = "Randomize";
+            this.BtnRandomize.UseVisualStyleBackColor = true;
+            this.BtnRandomize.Click += new System.EventHandler(this.BtnRandomize_Click);
+            // 
+            // LabelClicks
+            // 
+            this.LabelClicks.Location = new System.Drawing.Point(77, 167);
+            this.LabelClicks.Name = "LabelClicks";
+            this.LabelClicks.Size = new System.Drawing.Size(94, 34);
+            this.LabelClicks.TabIndex = 2;
+            this.LabelClicks.Text = "0";
+            this.LabelClicks.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BtnReset.Location = new System.Drawing.Point(80, 64);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(91, 38);
+            this.BtnReset.TabIndex = 3;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 446);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.LabelClicks);
+            this.Controls.Add(this.BtnRandomize);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel table;
+        private System.Windows.Forms.Button BtnRandomize;
+        private System.Windows.Forms.Label LabelClicks;
+        private System.Windows.Forms.Button BtnReset;
+    }
+}
+
---
+++ Puzzle/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    enum Side { Left, Top, Right, Bottom }
+
+    public partial class Form1 : Form
+    {
+        const int rows = 2;
+        const int columns = 2;
+        int clicks = 0;
+        TableLayoutPanelCellPosition emptyCellPosition =
+            new TableLayoutPanelCellPosition(columns - 1, rows - 1);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            table.Controls.Clear();
+            table.ColumnStyles.Clear();
+            table.RowStyles.Clear();
+            table.RowCount = rows;
+            table.ColumnCount = columns;
+            emptyCellPosition = new TableLayoutPanelCellPosition(columns - 1, rows - 1);
+            var cell = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                var rowStyle = new RowStyle(SizeType.Percent, 100 / rows);
+                table.RowStyles.Add(rowStyle);
+                for (int colurmn = 0; colurmn < columns; colurmn++)
+                {
+                    if (row == 0)
+                    {
+                        var columnStyle = new ColumnStyle(SizeType.Percent, 100 / columns);
+                        table.ColumnStyles.Add(columnStyle);
+
+                    }
+                    if (colurmn != columns - 1 || row != rows - 1)
+                    {
+                        var position = new TableLayoutPanelCellPosition(colurmn, row);
+                        var button = new Button
+                        {
+                            Name = "button" + ++cell,
+                            Text = cell.ToString(),
+                            Tag = position,
+                            Font = new Font("", rows * -3 + 32),
+                            Dock = DockStyle.Fill,
+                        };
+                        button.Click += button_Click;
+                        table.Controls.Add(button);
+                        table.SetCellPosition(button, position);
+                    }
+                }
+            }
+        }
+
+        private void Randomize()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < columns * rows; i++)
+            {
+                var cell1 = new TableLayoutPanelCellPosition(rand.Next(columns), rand.Next(rows));
+                var cell2 = new TableLayoutPanelCellPosition(rand.Next(columns), rand.Next(rows));
+                for (; ; )
+                {
+                    if (cell1 != emptyCellPosition && cell2 != emptyCellPosition)
+                    {
+
+                        var button1 = table.GetControlFromPosition(cell1.Column, cell1.Row);
+                        var button2 = table.GetControlFromPosition(cell2.Column, cell2.Row);
+                        table.SetCellPosition(button1, cell2);
+                        table.SetCellPosition(button2, cell1);
+                        Thread.Sleep(100);
+                        Application.DoEvents();
+                    }
+                    break;
+                }
+            }
+            clicks = 0;
+            LabelClicks.Text = clicks.ToString();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            MoveButton(sender as Control);
+        }
+
+        private List<Side> GetAllowedSides(TableLayoutPanelCellPosition position)
+        {
+            var allowed = new List<Side>();
+            if (position.Column != 0) allowed.Add(Side.Left);
+            if (position.Row != 0) allowed.Add(Side.Top);
+            if (position.Column != columns - 1) allowed.Add(Side.Right);
+            if (position.Row != rows - 1) allowed.Add(Side.Bottom);
+            return allowed;
+        }
+
+        private List<TableLayoutPanelCellPosition> getAllowedPositions
+            (TableLayoutPanelCellPosition current)
+        {
+            var allowedSides = GetAllowedSides(current);
+            var allowedPositions = allowedSides.ConvertAll(side => new TableLayoutPanelCellPosition
+            {
+                Column = current.Column + (side == Side.Left ? -1 : side == Side.Right ? 1 : 0),
+                Row = current.Row + (side == Side.Top ? -1 : side == Side.Bottom ? 1 : 0),
+            });
+            return allowedPositions;
+        }
+
+        private void MoveButton(Control button)
+        {
+            var position = table.GetCellPosition(button);
+            var positions = getAllowedPositions(position);
+            if (positions.Contains(emptyCellPosition))
+            {
+                var current = table.GetCellPosition(button);
+                table.SetCellPosition(button, emptyCellPosition);
+                emptyCellPosition = current;
+            }
+            LabelClicks.Text = (++clicks).ToString();
+            if (IsCorrect())
+            {
+                MessageBox.Show("Nice man!");
+            }
+        }
+
+        private void BtnRandomize_Click(object sender, EventArgs e)
+        {
+            Randomize();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private bool IsCorrect()
+        {
+            bool correct = true;
+            foreach (Button button in table.Controls)
+            {
+                if (table.GetCellPosition(button) != (TableLayoutPanelCellPosition)button.Tag)
+                {
+                    correct = false;
+                }
+            }
+            return correct;
+        }
+    }
+}
